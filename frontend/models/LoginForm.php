3b235aebfad5de4257bc0a9f1c9449b0be7f6831
LiHang\maikuraki <l541425845@gmail.com>
2017-05-01 06:22:46 +0800

    fix a login bug

--- frontend/models/LoginForm.php
+++ frontend/models/LoginForm.php
@@ -26,7 +26,7 @@ class LoginForm extends Model
     {
         return [
             // username and password are both required
-            [['username', 'password','repassword'], 'required'],
+            [['username', 'password'], 'required'],
             // rememberMe must be a boolean value
             ['rememberMe', 'boolean'],
             // password is validated by validatePassword()
