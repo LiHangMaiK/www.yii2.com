4c5a1d6f40688cef4195e24a2a1a28e66b2c3a75
LiHang\maikuraki <l541425845@gmail.com>
2017-06-12 22:13:42 +0800

    commit

---
+++ api/assets/AppAsset.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace api\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main backend application asset bundle.
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    //全局CSS
+    public $css = [
+        'css/site.css',
+    ];
+    //全局JS
+    public $js = [
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+    /**********下面两个方法，是自动加载CSS,JS文件的方法****************/
+    /* 视图中：\backend\assets\AppAsset::addCss($this,'@web/css/main.css');***********/
+
+    //定义按需加载JS方法，注意加载顺序在最后
+    public static function addScript($view, $jsfile) {
+        $view->registerJsFile($jsfile, [AppAsset::className(), "depends" => "api\assets\AppAsset"]);
+    }
+    //定义按需加载css方法，注意加载顺序在最后
+    public static function addCss($view, $cssfile) {
+        $view->registerCssFile($cssfile, [AppAsset::className(), "depends" => "api\assets\AppAsset"]);
+    }
+}
