d5467ef0b4b6c439935494bf1c4bdfce65a30002
LiHang\maikuraki <l541425845@gmail.com>
2017-06-20 01:35:16 +0800

    send mail

--- backend/components/Mail.php
+++ backend/components/Mail.php
@@ -4,6 +4,11 @@
  * User: maikuraki
  * Date: 2017/6/20
  * Time: 0:18
+ *
+ * 邮件类，通过事件来发送邮件
+ * 需要控制器下init方法绑定此类 $this->on(self::SEND_MAIL,['backend\components\Mail','sendMail']);
+ * 方法中 $this->trigger(SELF::SEND_MAIL,$event); 来触发
+ *
  */
 
 namespace backend\components;
@@ -16,9 +21,13 @@ class Mail
      * 发送邮件
      */
     public static function sendMail($event){
-        //接收事件参数
-        echo "email is {$event->email} <br>";
-        echo "subject is {$event->subject} <br>";
-        echo "content is {$event->content}";
+
+        $mail= Yii::$app->mailer->compose();
+        $mail->setTo($event->email); //要发送给那个人的邮箱
+        $mail->setSubject($event->subject); //邮件主题
+        $mail->setTextBody($event->content); //发布纯文字文本
+//        $mail->setHtmlBody("<br>问我我我我我");    //发布可以带html标签的文本
+
+        return $mail->send();
     }
 }
